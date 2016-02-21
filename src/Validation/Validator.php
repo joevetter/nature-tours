4ac1c58aec13cb8aa85da6c978c2fd8a1cc4de25
joevetter <joevetter@gmx.net>
2016-02-21 10:49:43 +0100

    added default to switch

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -11,40 +11,35 @@ class Validator
 
     foreach($validation_data as $element => $value)
     {
-      if(isset($_REQUEST[$element]))
-      {
-        $rules = explode('|', $value);
-        
-        foreach($rules as $rule)
-        {
-          $exploded = explode(':', $rule);
+      $rules = explode('|', $value);
 
-          switch($exploded[0])
-          {
-            case 'min':
-              if(false == Valid::stringType()->length($exploded[1])->Validate($_REQUEST[$element]))
-              {
-                $errors[] = $element . ' must be at least ' . $exploded[1] . ' characters long!';
-              }
-              break;
-            case 'email':
-              if(false == Valid::email()->Validate($_REQUEST[$element]))
-              {
-                $errors[] = $element . ' must be a valid e-mail address!';
-              }
-              break;
-            case 'equalTo':
-              if(false == Valid::equals($_REQUEST[$element])->Validate($_REQUEST[$exploded[1]]))
-              {
-                $errors[] = $element . ' does not match verification value!';
-              }
-              break;
-            default:
-              # nothing
-          }
+      foreach($rules as $rule)
+      {
+        $exploded = explode(':', $rule);
+
+        switch($exploded[0])
+        {
+          case 'min':
+            if(false == Valid::stringType()->length($exploded[1])->Validate($_REQUEST[$element]))
+            {
+              $errors[] = $element . ' must be at least ' . $exploded[1] . ' characters long!';
+            }
+            break;
+          case 'email':
+            if(false == Valid::email()->Validate($_REQUEST[$element]))
+            {
+              $errors[] = $element . ' must be a valid e-mail address!';
+            }
+            break;
+          case 'equalTo':
+            if(false == Valid::equals($_REQUEST[$element])->Validate($_REQUEST[$exploded[1]]))
+            {
+              $errors[] = $element . ' does not match verification value!';
+            }
+            break;
+          default:
+            $errors[] = $element . ' not found!';
         }
-      } else {
-        $errors[] = $element . ' not found!';
       }
     }
 
